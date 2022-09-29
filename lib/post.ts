16297e52ca275b542b7da5f99bd89dcf77ac5369
유일 <83910706+minumsa@users.noreply.github.com>
2022-09-29 11:57:21 +0900

    getStaticProps를 이용한 포스트 리스트 나열

--- lib/post.ts
+++ lib/post.ts
@@ -3,27 +3,36 @@ import path from 'path';
 import matter from 'gray-matter';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
+console.log('process.cwd()', process.cwd());
+// /Users/yooil/Downloads/next-typescript
+console.log('postDirectory', postsDirectory);
+// /Users/yooil/Downloads/next-typescript/posts
 
 export function getSortedPostsData() {
-  // /posts 파일 이름을 잡아주기
+  // /Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   // ['pre-rendering.md', ...];
-
+  console.log('fileNames', fileNames);
+  // fileNames [ 'pre-rendering.md', 'ssg-ssr.md' ]
   const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '');
 
+    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf-8');
 
+    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
 
+    // Combine the data with id
     return {
       id,
-      ...allPostsData(matterResult.data as { date: string; title: string }),
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
 
-  // Sorting
+  // Sort posts by data
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
